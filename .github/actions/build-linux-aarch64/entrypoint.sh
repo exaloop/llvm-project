a85a73e92b5c2f8e4f4ec1cc109a3b70201838f4
Ibrahim Numanagić <ibrahimpasa@gmail.com>
2025-07-30 15:17:04 +0200

    New CI

--- .github/actions/build-linux-aarch64/entrypoint.sh
+++ .github/actions/build-linux-aarch64/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh -l
 set -e
 
-cd /github/workspace
+WORKSPACE="${1:-/github/workspace}"
+echo "Workspace: ${WORKSPACE}"
+cd "$WORKSPACE"
+ls -lah
 
 export VERSION="$(git describe --tags --abbrev=0)"
-export FULLVERSION="$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)"
+export FULL_VERSION="$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)"
 export ARCH="$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
 
-cmake -S llvm-src/llvm -G Ninja \
-  -B llvm-src/llvm/build \
+cmake -S llvm -G Ninja \
+  -B llvm/build \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_ENABLE_RTTI=ON \
@@ -17,11 +20,11 @@ cmake -S llvm-src/llvm -G Ninja \
   -DLLVM_BUILD_TOOLS=OFF \
   -DLLVM_ENABLE_PROJECTS="clang;openmp"
 
-cmake --build llvm-src/llvm/build
+cmake --build llvm/build
 
-cmake --install llvm-src/llvm/build --prefix=/opt/llvm-codon
+cmake --install llvm/build --prefix=/opt/llvm-codon
 
-touch "/opt/llvm-codon/llvm-codon-${FULLVERSION}-${ARCH}.version"
+touch "/opt/llvm-codon/llvm-codon-${FULL_VERSION}-${ARCH}.version"
 
 tar cjvf llvm-${VERSION}-${ARCH}.tar.bz2 -C /opt llvm-codon/
 du -sh llvm-${VERSION}-${ARCH}.tar.bz2
--- .github/actions/build-linux-x86_64/entrypoint.sh
+++ .github/actions/build-linux-x86_64/entrypoint.sh
@@ -10,8 +10,8 @@ export VERSION="$(git describe --tags --abbrev=0)"
 export FULL_VERSION="$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)"
 export ARCH="$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
 
-cmake -S llvm-src/llvm -G Ninja \
-  -B llvm-src/llvm/build \
+cmake -S llvm -G Ninja \
+  -B llvm/build \
   -DCMAKE_BUILD_TYPE=Release \
   -DLLVM_INCLUDE_TESTS=OFF \
   -DLLVM_ENABLE_RTTI=ON \
@@ -20,9 +20,9 @@ cmake -S llvm-src/llvm -G Ninja \
   -DLLVM_BUILD_TOOLS=OFF \
   -DLLVM_ENABLE_PROJECTS="clang;openmp"
 
-cmake --build llvm-src/llvm/build
+cmake --build llvm/build
 
-cmake --install llvm-src/llvm/build --prefix=/opt/llvm-codon
+cmake --install llvm/build --prefix=/opt/llvm-codon
 
 touch "/opt/llvm-codon/llvm-codon-${FULL_VERSION}-${ARCH}.version"
 
