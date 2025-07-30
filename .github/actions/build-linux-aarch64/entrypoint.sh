eb25948b21d635bb57ef662ffc3e11fec0d81f2e
Ibrahim Numanagić <ibrahimpasa@gmail.com>
2025-07-30 14:47:47 +0200

    New CI

---
+++ .github/actions/build-linux-aarch64/entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/sh -l
+set -e
+
+cd /github/workspace
+
+export VERSION="$(git describe --tags --abbrev=0)"
+export FULLVERSION="$(git describe --tags --abbrev=0)-$(git rev-parse --short HEAD)"
+export ARCH="$(uname -s | tr '[:upper:]' '[:lower:]')-$(uname -m)"
+
+cmake -S llvm-src/llvm -G Ninja \
+  -B llvm-src/llvm/build \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DLLVM_INCLUDE_TESTS=OFF \
+  -DLLVM_ENABLE_RTTI=ON \
+  -DLLVM_ENABLE_ZLIB=OFF \
+  -DLLVM_TARGETS_TO_BUILD=all \
+  -DLLVM_BUILD_TOOLS=OFF \
+  -DLLVM_ENABLE_PROJECTS=clang
+
+cmake --build llvm-src/llvm/build
+
+cmake --install llvm-src/llvm/build --prefix=/opt/llvm-codon
+
+touch "/opt/llvm-codon/llvm-codon-${FULLVERSION}-${ARCH}.version"
+
+tar cjvf llvm-${VERSION}-${ARCH}.tar.bz2 -C /opt llvm-codon/
+du -sh llvm-${VERSION}-${ARCH}.tar.bz2
